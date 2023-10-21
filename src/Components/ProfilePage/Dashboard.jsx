ceaef8ecf0c06036ea57414299a1697146b79552
Ubais88 <moubais2143@gmail.com>
2023-10-21 22:10:41 +0530

    notes added

--- src/Components/ProfilePage/Dashboard.jsx
+++ src/Components/ProfilePage/Dashboard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Profile from './Profile'
-// import Notes from './Notes'
+import Notes from './Notes'
 // import Timer from './Timer'
 // import Weather from './Weather'
 // import News from './News.jsx'
 import Style from "./Dashboard.module.scss"
-// import Button from '../Common/Button'
-// import { useNavigate } from 'react-router-dom'
+import Button from '../Common/Button'
+import { useNavigate } from 'react-router-dom'
 
 
 const ProfilePage = () => {
@@ -17,28 +17,26 @@ const ProfilePage = () => {
   // }
 
   return (
-    // <div className={Style.mainBtn}>
-    //   <div className={Style.main}>
-    //     <div className={Style.tnpwContainer}>
-    //       <div className={Style.npwContainer}>
-    //         <div className={Style.profileweatherContainer}>
-    //             <div className={Style.profile}><Profile/></div>
-    //             {/* <div className={Style.weather}><Weather/></div> */}
-    //         </div>
-    //         {/* <div className={Style.notes}><Notes/></div> */}
-    //       </div>
-    //       {/* <div className={Style.timer}><Timer/></div> */}
-    //     </div>
-    //     <div className={Style.newssection}>
-    //       {/* <div className={Style.news}><News/></div> */}
-    //     </div>
-    //   </div>
-    //   <div className={Style.btn}>
-    //     {/* <Button Text={"Browse"} parentfunction={handlesubmit} /> */}
-    //   </div>
-    // </div>
-    <div className={Style.profile}><Profile/></div>
-
+    <div className={Style.mainBtn}>
+      <div className={Style.main}>
+        <div className={Style.tnpwContainer}>
+          <div className={Style.npwContainer}>
+            <div className={Style.profileweatherContainer}>
+                <div className={Style.profile}><Profile/></div>
+                {/* <div className={Style.weather}><Weather/></div> */}
+            </div>
+            <div className={Style.notes}><Notes/></div>
+          </div>
+          {/* <div className={Style.timer}><Timer/></div> */}
+        </div>
+        <div className={Style.newssection}>
+          {/* <div className={Style.news}><News/></div> */}
+        </div>
+      </div>
+      <div className={Style.btn}>
+        {/* <Button Text={"Browse"} parentfunction={handlesubmit} /> */}
+      </div>
+    </div>
   )
 }
 
